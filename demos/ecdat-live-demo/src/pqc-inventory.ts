c198bb90714e5ca1a798e035a427a4018497a843
Kaldeo1666 <kar16.studies@gmail.com>
2026-09-15 17:47:17 +0530

    chore: remove obsolete repository files

--- demos/ecdat-live-demo/src/pqc-inventory.ts
+++
@@ -1,4 +0,0 @@
-import { createHash, generateKeyPair } from "node:crypto";
-
-export const documentDigest = createHash("sha256").update("demo").digest("hex");
-generateKeyPair("rsa", { modulusLength: 2048 }, () => {});
